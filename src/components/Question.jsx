adf4f61c6400932665a7db567c6aa06f3f4d8ec7
Marek Belis <marek.belis810@gmail.com>
2024-12-19 09:29:58 +0100

    changed ui + quest. add

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,33 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function Question() {
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // Index aktuálnej otázky
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(375); // Index aktuálnej otázky
   const [answers, setAnswers] = useState({}); // Načítané odpovede
   const [questions, setQuestions] = useState([]); // Načítané otázky
   const [selectedOptions, setSelectedOptions] = useState({}); // Vybrané checkboxy
   const [results, setResults] = useState({}); // Uchováva stav správnych odpovedí
-  const options = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  const options = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
   // Načítaj odpovede a otázky pri načítaní stránky
   useEffect(() => {
-    fetch(`${import.meta.env.BASE_URL}assets/answr.txt`)
-    .then((res) => res.text())
-    .then((data) => {
-      const parsedAnswers = data.split("\n").reduce((acc, line) => {
-        if (line.trim() === "") return acc; // Preskočí prázdne riadky
-        const [question, answer] = line.trim().split(" ");
-        acc[parseInt(question)] = answer; // Priradí číslo otázky a odpoveď
-        return acc;
-      }, {});
-      setAnswers(parsedAnswers);
-    });
+    fetch(`${import.meta.env.BASE_URL}assets/B_odpovede.txt`)
+      .then((res) => res.text())
+      .then((data) => {
+        const parsedAnswers = data.split("\n").reduce((acc, line) => {
+          if (line.trim() === "") return acc; // Preskočí prázdne riadky
+          const [question, answer] = line.trim().split(" ");
+          acc[parseInt(question)] = answer; // Priradí číslo otázky a odpoveď
+          return acc;
+        }, {});
+        setAnswers(parsedAnswers);
+      });
 
-    fetch(`${import.meta.env.BASE_URL}assets/question.txt`)
+    fetch(`${import.meta.env.BASE_URL}assets/B_otazky.txt`)
       .then((res) => res.text())
       .then((data) => {
         // Rozdeľ na riadky
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-        
+        const lines = data.split("\n").filter((line) => line.trim() !== "");
+
         // Skupinovanie na bloky (otázka + 8 možností)
         const parsedQuestions = [];
         for (let i = 0; i < lines.length; i += 9) {
@@ -36,18 +36,18 @@ export default function Question() {
             console.log(`Skipping block due to incorrect line count:`, block);
             continue;
           }
-    
+
           // Prvý riadok je otázka
-          const questionText = block[0].replace(/^(\d+)\.\s*/, '');
-          const questionNumber = block[0].match(/^(\d+)\./)?.[1] || '';
-    
+          const questionText = block[0].replace(/^(\d+)\.\s*/, "");
+          const questionNumber = block[0].match(/^(\d+)\./)?.[1] || "";
+
           // Zvyšné riadky sú možnosti
           const questionOptions = block.slice(1).reduce((acc, line, index) => {
             const key = String.fromCharCode(97 + index); // Generuje 'a', 'b', 'c', ...
             acc[key] = line.trim();
             return acc;
           }, {});
-    
+
           // Ulož otázku do výsledku
           parsedQuestions.push({
             number: questionNumber,
@@ -55,8 +55,8 @@ export default function Question() {
             ...questionOptions,
           });
         }
-    
-        console.log('Parsed Questions:', parsedQuestions); // Debugging načítaných otázok
+
+        console.log("Parsed Questions:", parsedQuestions); // Debugging načítaných otázok
         setQuestions(parsedQuestions);
       });
   }, []);
@@ -93,60 +93,64 @@ export default function Question() {
   };
 
   return (
-    <div className="d-flex  flex-column">
+    <div className="d-flex  flex-column mainContainer">
       <div className="text-light ">
-  {questions.length > 0 ? (
-    <>
-      <h3>{`${currentQuestionIndex + 1}. ${questions[currentQuestionIndex]?.q || 'Načítavam otázky...'}`}</h3>
-      <div className="d-flex mt-4 flex-column ">
-        {Object.entries(questions[currentQuestionIndex] || {})
-          .filter(([key]) => key !== 'q' && key !== 'number')
-          .map(([key, value]) => (
-            <div
-              className="form-check d-flex align-items-center"
-              key={key}
-              style={{
-                backgroundColor: results[key.toUpperCase()] === "correct" ? "#198754" : "transparent",
-                borderRadius: "5px",
-                padding: "5px",
-              }}
-            >
-              <label
-                className="form-check-label text-light hvrOption"
-                htmlFor={`checkbox-${key}`}
-                style={{
-                  flex: "1", // Label zaberie dostupný priestor
-                  marginBottom: "0", // Text zarovnaný bez medzery
-                  wordBreak: "break-word", // Zalomí dlhý text
-                  textAlign: "left", // Text zarovnaný naľavo
-                }}
-              >
-                {value}
-              </label>
-              <input
-                className="form-check-input checkBoxLarge"
-                type="checkbox"
-                id={`checkbox-${key}`}
-                checked={!!selectedOptions[key.toUpperCase()]}
-                onChange={() => handleCheckboxChange(key.toUpperCase())}
-                style={{
-                  marginLeft: "10px", // Medzera medzi textom a checkboxom
-                }}
-              />
+        {questions.length > 0 ? (
+          <>
+            <h3>{`${currentQuestionIndex + 1}. ${
+              questions[currentQuestionIndex]?.q || "Načítavam otázky..."
+            }`}</h3>
+            <div className="d-flex mt-4 flex-column ">
+              {Object.entries(questions[currentQuestionIndex] || {})
+                .filter(([key]) => key !== "q" && key !== "number")
+                .map(([key, value]) => (
+                  <div
+                    className="form-check d-flex align-items-center p-1 mt-2"
+                    key={key}
+                    style={{
+                      backgroundColor:
+                        results[key.toUpperCase()] === "correct"
+                          ? "#198754"
+                          : "transparent",
+                      borderRadius: "5px",
+                    }}
+                  >
+                    <input
+                      className="form-check-input me-2 hvrOption"
+                      type="checkbox"
+                      id={`checkbox-${key}`}
+                      checked={!!selectedOptions[key.toUpperCase()]}
+                      onChange={() => handleCheckboxChange(key.toUpperCase())}
+                    />
+                    <label
+                      className="form-check-label hvrOpt"
+                      htmlFor={`checkbox-${key}`}
+                      style={{
+                        wordBreak: "break-word", // Zalomí dlhý text
+                      }}
+                    >
+                      {value}
+                    </label>
+                  </div>
+                ))}
             </div>
-          ))}
+          </>
+        ) : (
+          <h3>Načítavam otázky...</h3>
+        )}
       </div>
-    </>
-  ) : (
-    <h3>Načítavam otázky...</h3>
-  )}
-</div>
-
-
-      <button onClick={evaluateAnswers} type="button" className="btn btn-secondary mt-5">
+      <button
+        onClick={evaluateAnswers}
+        type="button"
+        className="btn btn-secondary mt-5"
+      >
         Vyhodnotiť
       </button>
-      <button onClick={loadNextQuestion} type="button" className="btn btn-warning mt-5">
+      <button
+        onClick={loadNextQuestion}
+        type="button"
+        className="btn btn-warning mt-5"
+      >
         Ďalšia otázka
       </button>
     </div>
