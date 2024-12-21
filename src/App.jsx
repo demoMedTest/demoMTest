9b94d1fe69f12800f73a90d8202df2d7b5ceced4
Marek Belis <marek.belis810@gmail.com>
2024-12-21 18:48:26 +0100

    ga add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import Question from './components/Question'
 import './App.css'
+import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-56FMP4MD64'); // Nahraď "TVOJ_TRACKING_ID" tvojím Tracking ID
+ReactGA.pageview(window.location.pathname + window.location.search); // Zaznamená načítanie stránky
 
 function App() {
   const [count, setCount] = useState(0)
