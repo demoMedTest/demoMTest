3560640b3f71ddafdcc3300a7731a4d665884b6f
Marek Belis <marek.belis810@gmail.com>
2025-01-05 15:15:49 +0100

    img fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='ftr'>
-        <div className='imgHolder'><img src="/logoMED.svg" alt="logoMED" className="logoIMG" height={100}/></div>
+        <div className='imgHolder'><img src={`${import.meta.env.BASE_URL}logoMED.svg`} alt="logoMED" className="logoIMG" height={100}/></div>
     <p className='credentials'>Vytvoril: Marek Belis 2024©</p>
     </div>
   )
