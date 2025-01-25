466ed44b503540d2277220069be2dc349ea8aa8f
Marek Belis <marek.belis810@gmail.com>
2025-01-25 15:28:43 +0100

    img info box fix

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -368,7 +368,6 @@ export default function Question() {
       </button>
       <div id="infoBox" className="toast show" role="alert" aria-live="assertive" aria-atomic="true">
   <div className="toast-header">
-    <img src="..." className="rounded me-2" alt="..."></img>
     <strong className="me-auto">INFO</strong>
     <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
   </div>
