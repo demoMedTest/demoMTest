c49d0e27a82b8ee9b81300207cea98dff930f0fc
Marek Belis <marek.belis810@gmail.com>
2025-01-28 13:57:42 +0100

    fix url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Dynamické nastavenie základnej cesty
-const basePath = process.env.NODE_ENV === "production" ? "/medTest/" : "/";
+const basePath = process.env.NODE_ENV === "production" ? "/demoMTest/" : "/";
 
 export default defineConfig({
   plugins: [react()],
