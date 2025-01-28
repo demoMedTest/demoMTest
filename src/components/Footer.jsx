1d55e82a906d6f91624f333b7d624a150f4f2867
Marek Belis <marek.belis810@gmail.com>
2025-01-28 13:22:54 +0100

    fix3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className='ftr'>
         <div className='imgHolder'><img src={`${import.meta.env.BASE_URL}logoMED.svg`} alt="logoMED" className="logoIMG" height={100}/></div>
-    <p className='credentials'>Vytvoril: Marek Belis 2024©</p>
+    <p className='credentials'>MB 2024©</p>
     </div>
   )
 }
