98199d0b478c27b62f9ac2ad79b5683f05556a5c
Marek Belis <marek.belis810@gmail.com>
2025-02-03 21:26:17 +0100

    pwa test

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import VitePWA from "vite-plugin-pwa";
 
 // Dynamické nastavenie základnej cesty
 const basePath = process.env.NODE_ENV === "production" ? "/demoMTest/" : "/";
 
 export default defineConfig({
-  plugins: [react()],
-  base: basePath,
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',  // Automatické aktualizácie service worker-a
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\/assets\/.*\.(?:txt)$/,  // Cachovanie súborov z assets priečinka
+            handler: 'CacheFirst', // Najskôr kontroluje cache, ak nie je, pošle požiadavku na server
+            options: {
+              cacheName: 'assets-cache',
+              expiration: {
+                maxEntries: 50,  // Maximálny počet položiek v cache
+                maxAgeSeconds: 60 * 60 * 24 * 30, // Maximálna doba uchovávania súborov (30 dní)
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  base: basePath,  // Dynamické nastavenie základnej cesty
 });
